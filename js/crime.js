d19042d565d0558d2c1d0feb477052c72ce05a93
NGGKI <kien.dv2012@gmail.com>
2022-03-02 11:34:21 -0800

    update js with all info for a song

--- js/crime.js
+++ js/crime.js
@@ -1,5 +1,6 @@
 // Crime Data Explorer API
 
-let apiKey = "psLU38DZVp60vWENcvahlA1IOKbuRCSgeL001v1g"
+/* let apiKey = "psLU38DZVp60vWENcvahlA1IOKbuRCSgeL001v1g"
 let endPoint = "https://crime-data-explorer.fr.cloud.gov/#""
 
+ */
--- js/script.js
+++ js/script.js
@@ -3,22 +3,57 @@
 // fetch 1
 
 // fetch 2 - Spotify - https://rapidapi.com/Glavier/api/spotify23/
-function fetchMusic() {
-  fetch("https://spotify23.p.rapidapi.com/search/?q=the%20killers&type=tracks&offset=0&limit=100&numberOfTopResults=5", {
-    "method": "GET",
-    "headers": {
+
+fetch(
+  "https://spotify23.p.rapidapi.com/search/?q=the%20killers&type=tracks&offset=0&limit=100&numberOfTopResults=5",
+  {
+    method: "GET",
+    headers: {
       "x-rapidapi-host": "spotify23.p.rapidapi.com",
-      "x-rapidapi-key": "13afb176d0msh1bebd3b48309acfp18e1bbjsn28d023f0b592"
-    }
+      "x-rapidapi-key": "13afb176d0msh1bebd3b48309acfp18e1bbjsn28d023f0b592",
+    },
+  }
+)
+  .then(function (response) {
+    return response.json();
   })
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data)
-      song = data.tracks.items[7].data.name
-      console.log(song)
-    })
-}
+  .then(function (data) {
+    console.log(data);
+
+    //random method trigger after seach function
+    let x = Math.floor(Math.random() * 100);
+    console.log(x);
+
+    //or use x = # of incident
+
+    //calling var for output of a song
+    var songName = data.tracks.items[x].data.name;
+    var artistName = data.tracks.items[x].data.artists.items[0].profile.name;
+    var albumImg =
+      data.tracks.items[x].data.albumOfTrack.coverArt.sources[0].url;
+    var albumName = data.tracks.items[x].data.albumOfTrack.name;
+    var songLink = data.tracks.items[x].data.albumOfTrack.sharingInfo.shareUrl;
+
+    //title of the song
+    console.log("song:" + data.tracks.items[x].data.name);
+    //artist name
+    console.log(
+      "artist: " + data.tracks.items[x].data.artists.items[0].profile.name
+    );
+    //album picture
+    console.log(
+      "img: " + data.tracks.items[x].data.albumOfTrack.coverArt.sources[0].url
+    );
+    // album name
+    console.log("album name: " + data.tracks.items[x].data.albumOfTrack.name);
+    //Album link
+    console.log(
+      "link: " + data.tracks.items[x].data.albumOfTrack.sharingInfo.shareUrl
+    );
+    //more song of artist info on Spotify
+    console.log(data.tracks.items[x].data.artists.items[0].uri);
+    // link of the song
+    console.log(data.tracks.items[x].data.uri);
+  });
 
 // render to page

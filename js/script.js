fd6ea7ece9b4c4b46a8b309604ee0a6d232548ac
Korey Luu <39642290+Ahrionic@users.noreply.github.com>
2022-03-03 10:40:37 -0800

    added music player and added keyword variabe to search for speicific band

--- js/script.js
+++ js/script.js
@@ -1,58 +1,95 @@
-// Search
 
-// fetch 1
-
-// fetch 2 - Spotify - https://rapidapi.com/Glavier/api/spotify23/
-
-fetch(
-  "https://spotify23.p.rapidapi.com/search/?q=the%20killers&type=tracks&offset=0&limit=100&numberOfTopResults=5",
-  {
-    method: "GET",
-    headers: {
-      "x-rapidapi-host": "spotify23.p.rapidapi.com",
-      "x-rapidapi-key": "13afb176d0msh1bebd3b48309acfp18e1bbjsn28d023f0b592",
-    },
-  }
-)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-
-    //random method trigger after seach function
-    let x = Math.floor(Math.random() * 100);
-    console.log(x);
-
-    //or use x = # of incident
-
-    //calling var for output of a song
-    let songName = data.tracks.items[x].data.name;
-    let artistName = data.tracks.items[x].data.artists.items[0].profile.name;
-    let albumImg = data.tracks.items[x].data.albumOfTrack.coverArt.sources[0].url;
-    let albumName = data.tracks.items[x].data.albumOfTrack.name;
-    let songLink = data.tracks.items[x].data.albumOfTrack.sharingInfo.shareUrl;
+// was suppose to be for user input
+// document.getElementById('btn').addEventListener('click', event =>{
+//   const sb = document.getElementById('categoryInput')
+//   let selectedOption = sb.options[sb.selectedIndex]
+//   const selectedValue = selectedOption.value
+//   let keyword
+//   let val = selectedValue
+//   console.log(val)
+//   if (val === 0) {
+//     keyword = 'The Killers'
+//   } else if (val === 1) {
+//     keyword = 'The Police'
+//   } else if (val === 2) {
+//     keyword = 'Motorhead'
+//   } else if (val === 3) {
+//     keyword = 'FireHouse'
+//   } else {
+//     keyword = 'Knife Party'
+//   }
+  
+//   console.log(val)
+//   return keyword
+// })
 
 
-    //title of the song
-    console.log("song:" + songName);
-    //artist name
-    console.log("artist: " + artistName);
-    //album picture
-    console.log("img: " + albumImg);
-    // album name
-    console.log("album name: " + albumName);
-    //Album link
-    console.log("link: " + songLink);
+const songPicker = () =>{
+    
+    const keyword = "The police"
+
+  fetch(
+    "https://spotify23.p.rapidapi.com/search/?q=" + keyword + "&type=tracks&offset=0&limit=100&numberOfTopResults=5",
+    {
+      method: "GET",
+      headers: {
+        "x-rapidapi-host": "spotify23.p.rapidapi.com",
+        "x-rapidapi-key": "13afb176d0msh1bebd3b48309acfp18e1bbjsn28d023f0b592",
+      },
+    })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+
+      //random method trigger after seach function
+      let x = Math.floor(Math.random() * 100);
+      console.log(x);
+
+      //or use x = # of incident
+
+      //calling var for output of a song
+      let songName = data.tracks.items[x].data.name;
+      let artistName = data.tracks.items[x].data.artists.items[0].profile.name;
+      let albumImg = data.tracks.items[x].data.albumOfTrack.coverArt.sources[0].url;
+      let albumName = data.tracks.items[x].data.albumOfTrack.name;
+      let codeSong = data.tracks.items[x].data.albumOfTrack.id;
+      let songLink = data.tracks.items[x].data.albumOfTrack.sharingInfo.shareUrl;
+      
+      //song player on page
+      const musicBox = `<iframe
+              src="https://open.spotify.com/embed/album/${codeSong}?utm_source=generator"
+              width="100%"
+              height="200"
+              frameborder="0"
+              allowfullscreen=""
+              allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+            ></iframe>`;
+
+      //title of the song
+      console.log("song:" + songName);
+      //artist name
+      console.log("artist: " + artistName);
+      //album picture
+      console.log("img: " + albumImg);
+      // album name
+      console.log("album name: " + albumName);
+      //Album link
+      console.log("link: " + songLink);
 
 
-    // render to page
-    document.getElementById('songTitle').innerHTML = songName
-    document.getElementById('bandName').innerHTML = artistName
-    document.getElementById('art').setAttribute('src', albumImg)
-    document.getElementById('albumTitle').innerHTML = albumName
-    // document.getElementById('spotifyLink').setAttribute('href', songLink)
-  })
-  .catch(err => console.error(err))
+      // render to page
+      document.getElementById('songTitle').innerHTML = songName
+      document.getElementById('bandName').innerHTML = artistName
+      document.getElementById('art').setAttribute('src', albumImg)
+      document.getElementById('albumTitle').innerHTML = albumName
+      document.getElementById("frame").innerHTML = musicBox;
+      // document.getElementById('spotifyLink').setAttribute('href', songLink)
+      
+    })
+}
+
+songPicker()
 
 
